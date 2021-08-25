4d8240ef6063e9644c2eb3cca5710a2c19b71703
Anthony Luo <anthonyluo100@gmail.com>
2021-08-25 16:16:36 -0400

    inoot

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Website in progress :D
         </p>
         <a
           className="App-link"
