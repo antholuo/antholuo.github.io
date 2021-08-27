1621feb4804bfe94bbb24e9132f0d86f4764f0fb
Anthony Luo <anthonyluo100@gmail.com>
2021-08-26 20:57:03 -0400

    working routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 
-import BrowserRouter from react-router-dom
+import ReactDOM from 'react-dom';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+
+import About from './About'
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -19,45 +22,58 @@ import NavDropdown from 'react-bootstrap/NavDropdown'
 
 function App() {
   return (
-    <div className="App">
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="home">Anthony Luo</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="professional-experience">Professional Experience</Nav.Link>
-              <NavDropdown title="Projects" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Nav>
-              <Nav.Link href="#deets">Resume</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. <br></br>
-          Website in progress :D
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/antholuo"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Go to my github
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="/">Anthony Luo</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="work">Professional Experience</Nav.Link>
+                <NavDropdown title="Projects" id="collasible-nav-dropdown">
+                  <NavDropdown.Item href="projects/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="projects/3.2">Another action</NavDropdown.Item>
+                  <NavDropdown.Item href="projects/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="projects">Separated link</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              <Nav>
+                <Nav.Link href="About">About</Nav.Link>
+                <Nav.Link href="Resume">Resume</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+
+
+        <Route path="/" exact component={Home} />
+        <Route path="/work" component={Work} />
+        <Route path="/about" component={About} />
+        <Route path="/resume" component={Resume} />
+        <Route path="/projects" component={Project} />
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+const Home = () => (
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <p>
+      Edit <code>src/App.js</code> and save to reload. <br></br>
+      Website in progress :D
+    </p>
+    <a
+      className="App-link"
+      href="https://github.com/antholuo"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Go to my github
+    </a>
+  </header>
+);
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+  
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'  
+
+import About from './About'  
 
 ReactDOM.render(
   <React.StrictMode>
