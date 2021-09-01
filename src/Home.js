2cd609443dafbe5c67176dd2ab0da941636facdb
Anthony Luo <anthonyluo100@gmail.com>
2021-09-01 18:05:44 -0400

    blurb

--- src/Home.js
+++ src/Home.js
@@ -58,9 +58,9 @@ class Home extends React.Component {
                         <Container>
                             <Row>
                                 <Col md={4}><br></br></Col>
-                                <Col md={2}><Button variant="outline-dark" as={Link} to="/work">Professional Experience</Button>
+                                <Col md={2}><Button variant="outline-dark" size="sm" as={Link} to="/work">Professional Experience</Button>
                                 </Col>
-                                <Col md={2}><Button variant="outline-dark" as={Link} to="/resume">Resume</Button>
+                                <Col md={2}><Button variant="outline-dark" size="" as={Link} to="/resume">Resume</Button>
                                 </Col>
                                 
                             </Row>
