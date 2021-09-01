a1abda039cb9e00a0806df4add6868a5e08f45c3
Anthony Luo <anthonyluo100@gmail.com>
2021-09-01 10:59:31 -0400

    about page with image and auto-sized columns

--- src/About.js
+++ src/About.js
@@ -1,7 +1,32 @@
-import React from 'react'  
-class About extends React.Component {  
-  render() {  
-    return <h1>About</h1>  
-  }  
-}  
-export default About  
+import React from 'react'
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image'
+
+import img from './media/pfp.jpg'
+
+class About extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="about-title">
+          <Container>
+            <p className="header">Hi! My name's anthony. Nice to meet you.</p>
+            <Row>
+              <Col xs={12} md={4}>
+                <Image src={img} roundedCircle fluid/>
+              </Col>
+              <Col xs={12} md={8}>
+                <p className="header">THIS IS SOME MORE STUFF ABOUT ME I'lL JUST KEEP TYPING UNTIL IT WORKS OUT YUP</p>
+              </Col>
+            </Row>
+          </Container>
+
+        </div>
+      </>
+    )
+  }
+}
+export default About
