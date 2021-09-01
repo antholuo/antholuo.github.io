27b60ea41067c18422b1defa537eaddea0c96df0
Christina <christinaytzhangroxs@gmail.com>
2021-08-31 20:39:07 -0400

    containers uwu

--- src/Work.js
+++ src/Work.js
@@ -44,7 +44,7 @@ class Work extends React.Component {
 
     render(){
         return (
-            <section>
+            <container>
                 <div class = "title">
                     <p class = "header" >Experiences </p>
                     <hr class = "title-header"></hr>
@@ -93,7 +93,7 @@ class Work extends React.Component {
                         <a href="url">Eindhoven |  http://startselect.com</a>
                     </div>
                 </div>
-            </section>
+            </container>
         )
     }
 }
