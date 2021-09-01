deb244b3801edc5e629e3f12e702942c4f8256c1
Anthony Luo <anthonyluo100@gmail.com>
2021-09-01 16:45:10 -0400

    added some work info

--- src/Work.js
+++ src/Work.js
@@ -48,38 +48,48 @@ class Work extends React.Component {
         return (
             <container>
                 <div class="title">
-                    <p class="header" >Experiences </p>
+                    <p class="header" >Work Experience</p>
                     <hr class="title-line"></hr>
 
-                    <p class="text"> aljkhsdfgashgjdfchkasd asdhgfasghdfkc aghdfas ghafgd kgh asfdghkf asd qada asd jaghmdfahk kahjgd kjahfgdkjas fa</p>
+                    <p class="text">Welcome! Feel free to take a look around. For projects and tabs, please use the navbar at the top of this page.</p>
 
                 </div>
 
                 <div class="box">
 
                     <div class="work">
-                        <p class="header">Startselect | Online Prepaid Services</p>
+                        <p class="header">Ciena Canada</p>
                         <hr class="body-line"></hr>
 
-                        <p> May 2016 - current </p>
+                        <p> September 2021 - December 2021 </p>
                     </div>
                     <div class="description">
-                        <p class="header"> Fulltime - Senior Web Developer </p>
-                        <p> Working as a Senior Devloper on a large european based webshop for digital games and gifrcards. The application is Laravel based and as a team we are working in a fully scrum way.</p>
-                        <a href="url">Eindhoven |  http://startselect.com</a>
+                        <p class="header"> Co-op: ASIC/FPGA Development </p>
+                        <p>
+                            I am currently working on the design and development of Ciena's next-generation Optical ASIC!
+                        </p>
+                        <a href="https://www.ciena.ca/">Ciena Canada |  https://www.ciena.ca/</a>
                     </div>
                 </div>
                 <div class="box">
                     <div class="work">
-                        <p class="header">The Secret Lab</p>
+                        <p class="header">Ciena Canada</p>
                         <hr class="body-line"></hr>
 
-                        <p> May 2016 - current </p>
+                        <p> January 2021 - April 2021 </p>
                     </div>
                     <div class="description">
-                        <p class="header">Fulltime - Senior Web Developer</p>
-                        <p>As a senior at The Secret Lab I work on projects like Kaartjes.nl, Clay.com and more. Using frameworks and technologies such as Laravel, Wordpress, Drupal 8 and ElasticSearch. I also introduced working with Git Flow and supporting my colleagues to write nice readable and reusable code.</p>
-                        <a href="url">Eindhoven |  http://startselect.com</a>
+                        <p class="header"> Co-op: Digital ASIC/FPGA Verification </p>
+                        <p>
+                            During my first four month co-op term with Ciena, I worked with the optical ASIC development and verification teams to provide design and verification tools. Some highlights include: <br></br>
+                            <ul>
+                                <li>Writing development tools in System Verilog</li>
+                                <li>Creating wrappers, design checks, and automation scripts in Python</li>
+                                <li>Integrating design checks into Jenkins builds using Bash and Makefiles</li>
+                                <li>Worked within a Fedora Linux environment_</li>
+                            </ul>
+                        </p>
+                        <a href="https://www.ciena.ca/">Ciena Canada |  https://www.ciena.ca/</a>
                     </div>
                 </div>
                 <div class="box">
